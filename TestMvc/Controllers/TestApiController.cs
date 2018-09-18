e2953176072c8350515a8b357053b8a4fe20d6a1
张经纬 <jingwei.zhang@ly.com>
2018-09-18 11:48:31 +0800

    重来

---
+++ TestMvc/Controllers/TestApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TestMvc.Controllers
+{
+    public class TestApiController : ApiController
+    {
+        public string Get()
+        {
+            var latest = new DateTime(1900, 1, 1);
+            DateTime.TryParse("2018-09-18 06:00:00", out latest);
+            var checkIn = new DateTime(1900, 1, 1);
+            DateTime.TryParse("2018-09-17", out checkIn);
+            var latestTime = latest - checkIn;
+            var show = (latestTime.Days * 24 + latestTime.Hours) * 100 + latestTime.Minutes;
+
+            return show.ToString();
+        }
+    }
+}
